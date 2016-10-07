1b74371678ab42f18b28b01887d9819b5e68e1e0
Keshan CHETTY <kchetty@e5r6p4.jnb.42.fr>
2016-10-07 08:57:55 +0200

    NORMED

--- includes/fract_ol.h
+++ includes/fract_ol.h
@@ -6,7 +6,7 @@
 /*   By: kchetty <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/27 12:00:45 by kchetty           #+#    #+#             */
-/*   Updated: 2016/10/07 08:36:51 by kchetty          ###   ########.fr       */
+/*   Updated: 2016/10/07 08:57:32 by kchetty          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,6 @@
 # define KB_RIGHT 124
 # define KB_R 15
 # define KB_L 37
-
 # define L_KB_ESC 65307
 # define L_KB_SPACE 32
 # define L_KB_UP 65362
@@ -39,40 +38,37 @@
 # define L_KB_LEFT 65363
 # define L_SCROLL_UP 4
 # define L_SCROLL_DOWN 5
- 
 # define ZOOM 1
-
 # define SCROLL_UP 5
 # define SCROLL_DOWN 4
-
 # define WIN_H 500
 # define WIN_W 700
 
-typedef struct	s_fract
+typedef struct		s_fract
 {
-	double		c_real;
-	double		c_im;
-	double		new_real;
-	double		new_im;
-	char		*str;
-	double		old_real;
-	double		old_im;
-	double		move_x;
-	double		move_y;
-	double		zoom;
-	double		win_w;
-	double		win_h;
-	double		point_x;
-	double		point_y;
-	int			lock_state;
-	double		mapped_point_x;
-	double		mapped_point_y;
-	int			oldx;
-	int			oldy;
+	double			c_real;
+	double			c_im;
+	double			new_real;
+	double			new_im;
+	char			*str;
+	double			old_real;
+	double			old_im;
+	double			move_x;
+	double			move_y;
+	double			zoom;
+	double			win_w;
+	double			win_h;
+	double			point_x;
+	double			point_y;
+	int				lock_state;
+	double			mapped_point_x;
+	double			mapped_point_y;
+	int				oldx;
+	int				oldy;
 
-}				t_fract;
+}					t_fract;
 
-typedef	struct	s_mlx
+typedef	struct		s_mlx
 {
 	void			*mlx;
 	void			*win;
@@ -80,7 +76,7 @@ typedef	struct	s_mlx
 	int				y;
 	void			*img;
 	int				**map;
-	char			*data;							
+	char			*data;
 	int				fd;
 	int				bpp;
 	unsigned int	color;
@@ -91,45 +87,62 @@ typedef	struct	s_mlx
 	int				endian;
 	int				maxiterations;
 
-}				t_mlx;
+}					t_mlx;
 
-typedef	struct	s_global
+typedef	struct		s_global
 {
-	t_mlx		mlx;
-	t_fract		fract;
+	t_mlx			mlx;
+	t_fract			fract;
 
-}				t_global;
+}					t_global;
 
-//julia.c
-void	julia(t_global *g);
+/*
+** julia.c
+*/
+void				julia(t_global *g);
 
-//mandelbrot.c
-void	mandelbrot(t_global *g);
+/*
+** mandelbrot.c
+*/
+void				mandelbrot(t_global *g);
 
-//main.c
-int		main(int argc, char **argv);
-void	init(t_global *g);
+/*
+** main.c
+*/
+int					main(int argc, char **argv);
+void				init(t_global *g);
 
-//controls.c
-int		key_press(int keycode, t_global *g);
-int		key_release(int keycode);
-int		quitwin(void);
+/*
+** controls.c
+*/
+int					key_press(int keycode, t_global *g);
+int					key_release(int keycode);
+int					quitwin(void);
 
-//mouse_hook.c
-int		mouse_hook(int mouse_move, int x, int y, t_global *g);
-int     mouse_move(int x, int y, t_global *g);
+/*
+** mouse_hook.c
+*/
+int					mouse_hook(int mouse_move, int x, int y, t_global *g);
+int					mouse_move(int x, int y, t_global *g);
 
-//colours.c
-void	get_colour(t_global *g, int i);
-void	get_colour2(t_global *g);
+/*
+** colours.c
+*/
+void				get_colour(t_global *g, int i);
+void				get_colour2(t_global *g);
 
-//draw.c
-void	draw(t_global *g);
+/*
+**draw.c
+*/
+void				draw(t_global *g);
 
-//fractal3.c
-void	burning_ship(t_global *g);
+/*
+** fractal3.c
+*/
+void				burning_ship(t_global *g);
 
-//redraw.c
-void	new_image(t_global *g);
-
+/*
+** redraw.c
+*/
+void				new_image(t_global *g);
 #endif
